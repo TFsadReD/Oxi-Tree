21ba38902646721f9ec4b1cb5be72fc45cda07da
TFsadReD <MaiK.HeliO@yandex.ru>
2026-08-13 19:19:52 +0300

    0.2.0 - вынос основной логики в отдельный файл и добавление счётчиков файлов и директорий

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-use std::fs;
+mod tree;
 
 fn main() {
-    let local_dir = fs::read_dir(".").unwrap();
-
-    println!(".");
-
-    for value in local_dir{
-        let value = value.unwrap();
-        let file_name = value.file_name();
-        let file_type = value.file_type().unwrap();
-
-        print!("├──");
-
-        if file_type.is_dir(){
-            println!("📁 {}/", file_name.display());
-        } else {
-            println!("📄 {}", file_name.display());
-        }
-    }
-
-    println!("└── Конец разметки.");
+    tree::display_tree();
 }
---
+++ src/tree.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+
+pub fn display_tree() {
+    let local_dir = fs::read_dir(".").unwrap();
+
+    let mut dirs_count = 0;
+    let mut files_count = 0;
+
+    println!(".");
+
+    for value in local_dir{
+        let value = value.unwrap();
+        let file_name = value.file_name();
+        let file_type = value.file_type().unwrap();
+
+        print!("├──");
+
+        if file_type.is_dir(){
+            dirs_count += 1;
+            println!("📁 {}/", file_name.display());
+        } else {
+            files_count += 1;
+            println!("📄 {}", file_name.display());
+        }
+    }
+
+    println!("└── {} 📁  |  {} 📄", dirs_count, files_count);
+}
