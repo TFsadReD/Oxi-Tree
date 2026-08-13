4bce2d07be5463960fea915a1a5d04c433305c2d
TFsadReD <MaiK.HeliO@yandex.ru>
2026-08-13 18:53:15 +0300

    0.1.1 - создание проекта и первый рабочий вывод с различием папок/файлов

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let local_dir = fs::read_dir(".").unwrap();
+
+    for value in local_dir{
+        let value = value.unwrap();
+        let file_name = value.file_name();
+        let file_type = value.file_type().unwrap();
+
+        if file_type.is_dir(){
+            println!("📁 {}/", file_name.display());
+        } else {
+            println!("📄 {}", file_name.display());
+        }
+    }
+}
