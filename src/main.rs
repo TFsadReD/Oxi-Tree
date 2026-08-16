8853df9233f6dfa4835824b2b70a145c1419128a
TFsadReD <MaiK.HeliO@yandex.ru>
2026-08-17 00:43:43 +0300

    1.2.0 - теперь oxi-tree принимает аргумент пути

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,22 @@
 //! древовидной структуры директорий
 
 mod tree;
+use std::env;
 use std::path::Path;
 
 fn main() {
-    tree::display_tree(Path::new("."));
+    let path_arg = env::args().nth(1).unwrap_or_else(|| ".".to_string());
+    let path = Path::new(&path_arg);
+
+    if !path.exists() {
+        eprintln!("Ошибка: путь '{}' не существует!", path.display());
+        std::process::exit(1);
+    }
+
+    if !path.is_dir() {
+        eprintln!("Ошибка: '{}' не является директорией!", path.display());
+        std::process::exit(2);
+    }
+
+    tree::display_tree(path);
 }
